2288d8d7e45cedb92589d562e57f2ed4637608e3
AbrashnkovNikolai <146152673+AbrashnkovNikolai@users.noreply.github.com>
2025-05-28 18:25:15 +0500

    code rewiewed

--- Program.cs
+++ Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 
-// Базовый класс
+// Базовый класс StringHolder.
 public class StringHolder
 {
+    // Защищенное поле для хранения строки.
     protected string _value;
 
-    // Конструктор
+    // Конструктор для инициализации строки.
     public StringHolder(string value)
     {
         _value = value;
     }
 
-    // Конструктор копирования
+    // Конструктор копирования для создания копии объекта.
     public StringHolder(StringHolder other)
     {
         _value = other._value;
     }
 
-    // Метод, создающий строку из первого и последнего символов поля
+    // Метод для получения строки из первого и последнего символов.
     public string GetFirstAndLastChars()
     {
         if (_value.Length < 2)
@@ -27,63 +28,67 @@ public class StringHolder
         return _value[0] + "" + _value[_value.Length - 1];
     }
 
-    // Перегруженный метод ToString()
+    // Перегрузка метода ToString для вывода строки.
     public override string ToString()
     {
         return $"StringHolder: {_value}";
     }
 }
 
-// Дочерний класс
+// Дочерний класс PasswordHolder.
 public class PasswordHolder : StringHolder
 {
+    // Член для хранения длины пароля.
     private int _length;
 
-    // Конструктор
+    // Конструктор для инициализации пароля и длины.
     public PasswordHolder(string password, int length) : base(password)
     {
         _length = length;
     }
 
-    // Конструктор копирования
+    // Конструктор копирования 
     public PasswordHolder(PasswordHolder other) : base(other)
     {
         _length = other._length;
     }
 
-    // Метод, проверяющий, является ли пароль достаточно длинным
+    // Метод для проверки длины пароля.
     public bool IsPasswordLongEnough()
     {
         return _value.Length >= _length;
     }
 
-    // Метод, маскирующий пароль звездочками
+    // Метод для маскировки пароля.
     public string MaskPassword()
     {
         return new string('*', _value.Length);
     }
 
-    // Перегруженный метод ToString()
+    // Перегрузка метода ToString для вывода пароля и длины.
     public override string ToString()
     {
         return $"PasswordHolder: {_value} (length: {_length})";
     }
 }
 
-// Класс для ввода пароля с маскировкой
+// Класс для ввода пароля с маскировкой.
 public class PasswordInput
 {
+    
     private string _password;
-    private int _minLength; // Изменяем название переменной на _minLength
 
-    // Конструктор
-    public PasswordInput(int minLength) // Параметр теперь minLength
+    
+    private int _minLength;
+
+    // Конструктор для инициализации минимальной длины пароля.
+    public PasswordInput(int minLength)
     {
         _minLength = minLength;
         _password = "";
     }
 
-    // Метод для ввода пароля
+
     public void InputPassword()
     {
         do
@@ -112,38 +117,42 @@ public class PasswordInput
             Console.WriteLine();
 
             // Проверка длины пароля
-            if (_password.Length < _minLength) // Изменяем условие на < _minLength
+            if (_password.Length < _minLength)
             {
                 Console.WriteLine($"Пароль должен содержать минимум {_minLength} символов. Попробуйте еще раз.");
             }
-        } while (_password.Length < _minLength); // Повторяем ввод, пока пароль недостаточно длинный
+        } while (_password.Length < _minLength);
     }
 
-    // Метод для получения введенного пароля
+    // Метод для получения введенного пароля.
     public string GetPassword()
     {
         return _password;
     }
 
-    // Метод для вывода пароля
+    
     public void ShowPassword()
     {
         Console.WriteLine($"Введенный пароль: {_password}");
     }
 
-    // Метод для маскированного вывода пароля
+    
     public string MaskPassword()
     {
         return new string('*', _password.Length);
     }
 }
 
-
+// Класс времени 
 public class Time
 {
+    
     private byte hours;
+
+    
     private byte minutes;
 
+    // Конструктор для инициализации часов и минут.
     public Time(byte hours, byte minutes)
     {
         this.hours = hours;
@@ -151,6 +160,7 @@ public class Time
         NormalizeTime();
     }
 
+    // Метод для нормализации времени.
     private void NormalizeTime()
     {
         if (minutes >= 60)
@@ -161,6 +171,7 @@ public class Time
         hours %= 24; // Часы должны быть в диапазоне от 0 до 23
     }
 
+    // Перегрузка оператора сложения для добавления минут.
     public static Time operator +(Time time, uint minutesToAdd)
     {
         uint totalMinutes = (uint)(time.minutes + minutesToAdd);
@@ -169,21 +180,20 @@ public class Time
         return new Time(newHours, newMinutes);
     }
 
+    // Перегрузка оператора вычитания для вычитания минут.
     public static Time operator -(Time time, uint minutesToSubtract)
     {
-        // Общее количество минут, которое нужно вычесть
         int totalMinutes = time.minutes - (int)minutesToSubtract;
-        // Определяем новые часы и минуты
+        
         byte newHours = time.hours;
         byte newMinutes;
 
         // Если totalMinutes отрицательные, корректируем часы и минуты
         if (totalMinutes < 0)
         {
-            // Вычисляем, сколько часов нужно вычесть
             int hoursToSubtract = (int)Math.Ceiling((double)(-totalMinutes) / 60);
-            newHours = (byte)((newHours - hoursToSubtract + 24) % 24); // Корректируем часы
-            newMinutes = (byte)((totalMinutes + 60 * hoursToSubtract) % 60); // Корректируем минуты
+            newHours = (byte)((newHours - hoursToSubtract + 24) % 24); 
+            newMinutes = (byte)((totalMinutes + 60 * hoursToSubtract) % 60); 
         }
         else
         {
@@ -192,30 +202,37 @@ public class Time
 
         return new Time(newHours, newMinutes);
     }
+
+    // Перегрузка оператора инкремента для добавления одной минуты.
     public static Time operator ++(Time time)
     {
         return time + 1; // Увеличиваем на 1 минуту
     }
 
-    // Перегрузка оператора декремента
+    // Перегрузка оператора декремента для вычитания одной минуты.
     public static Time operator --(Time time)
     {
         return time - 1; // Уменьшаем на 1 минуту
     }
+
+    // Неявное приведение к типу byte для получения часов.
     public static implicit operator byte(Time time)
     {
         return time.hours;
     }
 
+    // Неявное приведение к типу bool для проверки времени.
     public static implicit operator bool(Time time)
     {
         return time.hours != 0 || time.minutes != 0;
     }
 
+    // Перегрузка метода ToString для вывода времени.
     public override string ToString()
     {
         return $"{hours:D2}:{minutes:D2}";
     }
+
 
     public static byte GetValidByteInput(string prompt, byte maxValue)
     {
@@ -231,6 +248,8 @@ public class Time
         }
     }
 
+    // Метод для получения валидного входного значения uint.
+    // Параметр: prompt - строка, выводимая в консоль.
     public static uint GetValidUIntInput(string prompt)
     {
         uint value;
@@ -247,7 +266,6 @@ public class Time
 }
 
 
-// Класс для тестирования
 class Program
 {
     static void Main(string[] args)
@@ -267,6 +285,7 @@ class Program
         {
             Console.WriteLine($"Маскированный пароль: {passwordInput.MaskPassword()}");
         }
+
         byte hours = Time.GetValidByteInput("Введите часы (0-23): ", 23);
         byte minutes = Time.GetValidByteInput("Введите минуты (0-59): ", 59);
 
